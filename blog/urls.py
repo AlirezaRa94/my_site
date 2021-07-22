596e33f3d9171a63c528a01aa2dfc68eb9ac4636
Alireza Raei <alirezaraei17@gmail.com>
2021-07-22 21:10:27 +0430

    Makes our site dynamic

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.starting_page, name='starting-page'),
     path('posts', views.posts, name='posts-page'),
-    path('posts/<slug:post_name>', views.post_details, name='post-details-page')
+    path('posts/<slug:slug>', views.post_details, name='post-details-page')
 ]
