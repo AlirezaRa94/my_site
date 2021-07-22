258037dfd15b3a4835e8833ace41c83fab675dc9
Alireza Raei <alirezaraei17@gmail.com>
2021-07-22 15:46:16 +0430

    added urls and views for blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.urls.conf import path
 
-# Create your views here.
+
+def starting_page(request):
+    pass
+
+
+def posts(request):
+    pass
+
+
+def post_details(request, post_name):
+    pass
